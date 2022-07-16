7cf3b9e7c23b519fe42c003aa8e9230ca151301f
steve_park <diasm2@gmail.com>
2022-07-16 20:29:50 +0700

    [delete]dump trash js

--- src/users/users.service.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { Users } from './entities/user.entity';
-import { UserRepository } from './users.repository';
-
-@Injectable()
-export class UsersService {}
