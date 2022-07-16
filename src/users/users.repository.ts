4fb029ccd3bfc04dd2e99cd9a2f3859407166035
steve_park <diasm2@gmail.com>
2022-07-16 20:30:20 +0700

    [fix]minor fix (deleted wrong code)

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -4,19 +4,7 @@ import { Users } from './entities/user.entity';
 
 @Injectable()
 export class UserRepository extends Repository<Users> {
-   /* 댓글 업로드 퀘스트 수행 */
    constructor(private dataSource: DataSource) {
       super(Users, dataSource.createEntityManager());
    }
-   async createComment(
-      playerId: number,
-      feedId: number,
-      comment: string
-   ): Promise<Users | undefined> {
-      // return await this.createQueryBuilder()
-      return undefined;
-   }
-
-   /* 댓글 삭제 */
-   async deleteComment(commentId: number) {}
 }
